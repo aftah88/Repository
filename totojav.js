d38e3599ee42d1ebbff391c5a3a2c4b964f5803d
aftah88 <72744161+aftah88@users.noreply.github.com>
2020-10-13 20:35:06 +0800

    Delete totojav.js

--- totojav.js
+++
@@ -1,5 +0,0 @@
-document.write('<a href="http://mobile.dewi365idn.com/?ref=brav77" style="text-decoration: none" target="_blank"> <img src="https://1.bp.blogspot.com/-zIRbmWY1CUM/X4R08ho9SRI/AAAAAAAAMYk/WpPulA11UbcbmzX9okCL7d2CfRZsSt0WQCLcBGAsYHQ/s0/dewi365.png" alt="DEWI365" title="DEWI365" style="width:140px;height:40px;border:0" /></a>
-
-<a href="http://wap.luwakcoffe.com/?cd=brav77" style="text-decoration: none" target="_blank"> <img src="https://1.bp.blogspot.com/-uDpF3h2HZu4/X4R07bZVRlI/AAAAAAAAMYQ/U_JKa8UfGM08-xsbDwFrovm_npCVL9a4QCLcBGAsYHQ/s0/bso118.png" alt="SINGAPORE 23 HADIAH" title="BSO 118" style="width:140px;height:40px;border:0" />
-
-<a href="http://103.10.200.53/user/link_ref/JOKER75" style="text-decoration: none" target="_blank"> <img src="https://3.bp.blogspot.com/-Sz8F7iZUjZc/VbcaJSKvsTI/AAAAAAAAACM/h61fVuarE0o/s1600/rgocasino.png" alt="RGOCASINO" title="RGOCASINO" style="width:140px;height:40px;border:0"></a>')// JavaScript Document
